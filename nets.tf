df18be3c9984f2d44e3ea9d82405a63a0a1692d8
Travis Marks <travis.marks@byu.edu>
2024-04-01 12:44:02 -0600

    active nets

--- nets.tf
+++ nets.tf
@@ -2,22 +2,26 @@ resource "okta_network_zone" "net_byup" {
   name     = "BYU Provo"
   type     = "IP"
   gateways = ["128.187.0.0/16"]
+  status = "ACTIVE"
 }
 
 resource "okta_network_zone" "net_byui" {
   name     = "BYU Idaho"
   type     = "IP"
   gateways = ["157.201.0.0/16"]
+  status = "ACTIVE"
 }
 
 resource "okta_network_zone" "net_byuh" {
   name     = "BYU Hawaii"
   type     = "IP"
   gateways = ["216.228.240.0/20"]
+  status = "ACTIVE"
 }
 
 resource "okta_network_zone" "net_church" {
   name     = "Church Net"
   type     = "IP"
   gateways = ["216.49.176.0/20"]
+  status = "ACTIVE"
 }
